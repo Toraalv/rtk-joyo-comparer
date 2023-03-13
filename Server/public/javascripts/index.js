8713d43ec6e9dbb990ddee4ad52ac3872f6b61f9
Mackaan <cocopew.02@gmail.com>
2023-03-13 01:33:30 +0100

    index.css? Ja. index.js? Nej. Idiot. Godnatt
    Knulla inte runt sådär Arvid, att kalla funktioner på det sättet är receptet för dålig prestanda

--- Server/public/javascripts/index.js
+++ Server/public/javascripts/index.js
@@ -1,43 +1,77 @@
-let qtyLearnt = 2200; // Hämtas sedan från användaren indata
+let _qtyLearnt = 0; // Hämtas sedan från användaren indata
+
+let _rtkList; // yabai
+let _joyoList = new Array(); // Detta kan du gott och väl optimera för morgondagen
 
 $(async function()	
 {
 	// Hämta listan av Heisigs kanji (sorterad, så index+1 == koohiis index)
-	let rtkList;
 	await $.get("/static/data/rtk1_index.txt", (data) => {
-		rtkList = (data.split("\n")); // Mannen tänk nu på att arrays börjar på 0 (0-2199)
+		_rtkList = (data.split("\n")); // Mannen tänk nu på att arrays börjar på 0 (0-2199)
 	});
-	resetRtkList(rtkList);
-	setLearntRtkList(rtkList, qtyLearnt);
+	resetRtkList(_rtkList);
+	setLearntRtkList(_rtkList, _qtyLearnt);
 
 	// Hämta listan av joyo kanji
-	let joyoList = new Array();
 	await $.get("/static/data/joyo_list_extended.json", (data) => {
-		joyoList = data;
+		_joyoList = data;
 	});
-	resetJoyoList(joyoList);
+	resetJoyoList(_joyoList);
 
-	console.log(compareRtkJoyo(rtkList, joyoList));
-	console.log(joyoList.grade);
+	console.log(compareRtkJoyo(_rtkList, _joyoList));
+	console.log(_joyoList.grade);
 
-	///////////// Bygga bygga
-	for (let i = 1; i < joyoList.grade.length; i++) {
+	/////////// Bygga bygga
+	for (let i = 1; i < _joyoList.grade.length; i++) {
 		let grade_section = $("<section>").addClass("grade");
 		let grade_h2 = $("<h3>").text(`Grade ${i == 7 ? "7-12" : i}`);
 		let grade_kanji_container = $("<div>").addClass("kanji_container");
 
 		grade_section.append(grade_h2);
-		for (let j = 0; j < joyoList.grade[i].length; j++) { // Det vackraste jag sett
-			grade_kanji_container.append(`<span class="kanji_item ${joyoList.grade[i][j].isActive ? 'kanji_item_active' : ''}">
-											<a href='//${joyoList.grade[i][j].jishoHref}'>
-												${joyoList.grade[i][j].kanji}
+		for (let j = 0; j < _joyoList.grade[i].length; j++) { // Det vackraste jag sett
+			grade_kanji_container.append(`<span class="kanji_item ${_joyoList.grade[i][j].isActive ? 'kanji_item_active' : ''}">
+											<a href='//${_joyoList.grade[i][j].jishoHref}'>
+												${_joyoList.grade[i][j].kanji}
 											</a>
 										</span>`);
 		}
 		grade_section.append(grade_kanji_container);
-		$("main").append(grade_section);
+		$("#grade_list").append(grade_section);
 	}
 });
+
+function badTomfoolery(element) {
+	element.form.amountInput.value = element.value;
+	_qtyLearnt = element.value;
+	console.log("この世界を再建する", _qtyLearnt);
+	rebuildThisWorld(element.value);
+}
+
+function rebuildThisWorld() {
+	resetRtkList(_rtkList);
+	setLearntRtkList(_rtkList, _qtyLearnt);
+	resetJoyoList(_joyoList); // Istället för att köra denna borde du göra en egen funktion som bara resettar "isActive"
+	compareRtkJoyo(_rtkList, _joyoList);
+
+	$("#grade_list").empty();
+	///////////// Bygga bygga
+	for (let i = 1; i < _joyoList.grade.length; i++) {
+		let grade_section = $("<section>").addClass("grade");
+		let grade_h2 = $("<h3>").text(`Grade ${i == 7 ? "7-12" : i}`);
+		let grade_kanji_container = $("<div>").addClass("kanji_container");
+
+		grade_section.append(grade_h2);
+		for (let j = 0; j < _joyoList.grade[i].length; j++) { // Det vackraste jag sett
+			grade_kanji_container.append(`<span class="kanji_item ${_joyoList.grade[i][j].isActive ? 'kanji_item_active' : ''}">
+											<a href='//${_joyoList.grade[i][j].jishoHref}'>
+												${_joyoList.grade[i][j].kanji}
+											</a>
+										</span>`);
+		}
+		grade_section.append(grade_kanji_container);
+		$("#grade_list").append(grade_section);
+	}
+}
 
 // Hjälp funktioner, かな
 function resetRtkList(rtkList) {
@@ -63,24 +97,25 @@ function setLearntRtkList(rtkList, range) {
 
 function compareRtkJoyo(rtkList, joyoList) {
 	let qtyRtkExisted = 0;
-	for (let rtk_i = 0; rtk_i < qtyLearnt; rtk_i++) {
+	for (let rtk_i = 0; rtk_i < _qtyLearnt; rtk_i++) {
 		for (let grade_i = 1; grade_i < joyoList.grade.length; grade_i++) {
 			for (let kanji_i = 0; kanji_i < joyoList.grade[grade_i].length; kanji_i++) {
 				if (rtkList[rtk_i].kanji == joyoList.grade[grade_i][kanji_i].kanji) {
 					rtkList[rtk_i].learnt = false;
 					joyoList.grade[grade_i][kanji_i].isActive = true;
+					console.log("lmao");
 					qtyRtkExisted++;
 				}
 			}
 		}
 	}
 	let rtkNotInJoyo = new Array();
-	for (let i = 0; i < qtyLearnt; i++) {
+	for (let i = 0; i < _qtyLearnt; i++) {
 		if (rtkList[i].learnt == true)
 			rtkNotInJoyo.push(rtkList[i].kanji);
 	}
-	if (qtyRtkExisted + rtkNotInJoyo.length != qtyLearnt) {
-		console.log("qtyRtkExisted: " + qtyRtkExisted + " + rtkNotInJoyo: " + rtkNotInJoyo.length + " does not equal qtyLearnt: " + qtyLearnt);
+	if (qtyRtkExisted + rtkNotInJoyo.length != _qtyLearnt) {
+		console.log("qtyRtkExisted: " + qtyRtkExisted + " + rtkNotInJoyo: " + rtkNotInJoyo.length + " does not equal qtyLearnt: " + _qtyLearnt);
 		return 1;
 	}
 	return {qtyRtkExisted, rtkNotInJoyo};
