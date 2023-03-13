6b968b33115ff1283d1db0ad0f581cb715fbbc59
Mackaan <cocopew.02@gmail.com>
2023-03-14 00:50:10 +0100

    Slider fungerar
    Användaren kan välja hur långt man kommit i RTK1 nu. Koden är okej, kan säkert förbättras, för det chuggar en del när man drar i slidern. Har även utökat joyoList med ett heisig index, vilket jag tror jag kan använda för optimering

--- Helpers/RTK1_To_Joyo/main.py
+++ Helpers/RTK1_To_Joyo/main.py
@@ -6,12 +6,13 @@ joyoFile = open("joyo_list.json", "r", encoding="utf8")
 rtkList = json.loads(rtkFile.read())
 joyoList = json.loads(joyoFile.read())
 
-for rtkObj in rtkList:
+for rtkObj_i, rtkObj in enumerate(rtkList):
 	for joyoGrade_i, joyoGrade in enumerate(joyoList["grade"]):
 		for joyoKanji_i, joyoKanji in enumerate(joyoGrade):
 			if rtkObj["kanji"] == joyoKanji:
 				# joyoKanji = rtkObj	funka inte?
 				joyoList["grade"][joyoGrade_i][joyoKanji_i] = rtkObj
+				joyoList["grade"][joyoGrade_i][joyoKanji_i]["heisigIndex"] = rtkObj_i + 1
 
 joyoExtendedFile = open("joyo_list_extended.json", "w", encoding="utf8")
 joyoExtendedFile.write(json.dumps(joyoList, ensure_ascii=False, indent=4))
--- Server/public/javascripts/index.js
+++ Server/public/javascripts/index.js
@@ -1,77 +1,79 @@
-let _qtyLearnt = 0; // Hämtas sedan från användaren indata
-
-let _rtkList; // yabai
-let _joyoList = new Array(); // Detta kan du gott och väl optimera för morgondagen
+//let _qtyLearnt = 0; // Hämtas sedan från användaren indata
 
 $(async function()	
 {
+	let _qtyLearnt = 0;
+
 	// Hämta listan av Heisigs kanji (sorterad, så index+1 == koohiis index)
+	let rtkList;
 	await $.get("/static/data/rtk1_index.txt", (data) => {
-		_rtkList = (data.split("\n")); // Mannen tänk nu på att arrays börjar på 0 (0-2199)
+		rtkList = (data.split("\n")); // Mannen tänk nu på att arrays börjar på 0 (0-2199)
 	});
-	resetRtkList(_rtkList);
-	setLearntRtkList(_rtkList, _qtyLearnt);
+	resetRtkList(rtkList);
+	// setLearntRtkList(rtkList, _qtyLearnt); // Ska inte köras här i slutändan
 
 	// Hämta listan av joyo kanji
-	await $.get("/static/data/joyo_list_extended.json", (data) => {
-		_joyoList = data;
+	let joyoList = new Array();
+	await $.get("/static/data/joyo_list_extended_V2.json", (data) => {
+		joyoList = data;
 	});
-	resetJoyoList(_joyoList);
+	resetJoyoList(joyoList);
 
-	console.log(compareRtkJoyo(_rtkList, _joyoList));
-	console.log(_joyoList.grade);
+	// compareRtkJoyo(rtkList, joyoList, _qtyLearnt); // Detta ska heller inte köras i slutändan
+	// console.log(joyoList.grade);
 
-	/////////// Bygga bygga
-	for (let i = 1; i < _joyoList.grade.length; i++) {
+	///////////// Bygga bygga				// Kanske inte heller ska vara här i slutändan? 				// Fast jo vänta lite, den måste ju visa någonting på sidan, bara att inga är lärda än
+	for (let i = 1; i < joyoList.grade.length; i++) {
 		let grade_section = $("<section>").addClass("grade");
 		let grade_h2 = $("<h3>").text(`Grade ${i == 7 ? "7-12" : i}`);
 		let grade_kanji_container = $("<div>").addClass("kanji_container");
 
 		grade_section.append(grade_h2);
-		for (let j = 0; j < _joyoList.grade[i].length; j++) { // Det vackraste jag sett
-			grade_kanji_container.append(`<span class="kanji_item ${_joyoList.grade[i][j].isActive ? 'kanji_item_active' : ''}">
-											<a href='//${_joyoList.grade[i][j].jishoHref}'>
-												${_joyoList.grade[i][j].kanji}
+		for (let j = 0; j < joyoList.grade[i].length; j++) { // Det vackraste jag sett
+			grade_kanji_container.append(`<span class="kanji_item ${joyoList.grade[i][j].isActive ? 'kanji_item_active' : ''}" id="${joyoList.grade[i][j].kanji}">
+											<a href='//${joyoList.grade[i][j].jishoHref}'>
+												${joyoList.grade[i][j].kanji}
 											</a>
 										</span>`);
 		}
 		grade_section.append(grade_kanji_container);
 		$("#grade_list").append(grade_section);
 	}
+
+	// Får denna ligga här? Hmmmm
+	$('[name^="qtyLearnt"]').on("input", (e) => { // Så denna skiten uppdaterar vilka kanjin som ska vara "aktiva" LÖPANDE (optimering i väggen)
+		_qtyLearnt = e.currentTarget.value;
+		
+		// resetRtkList(rtkList); // WTF, VARFÖR SKA INTE DENNA VAR HÄR????????????????????????????????????????????????
+		setLearntRtkList(rtkList, _qtyLearnt);
+		resetJoyoList(joyoList);
+		compareRtkJoyo(rtkList, joyoList, _qtyLearnt)
+		
+
+		for (let grade_i = 1; grade_i < joyoList.grade.length; grade_i++) {
+			for (let kanji_i = 0; kanji_i < joyoList.grade[grade_i].length; kanji_i++) {
+				if (joyoList.grade[grade_i][kanji_i].isActive) {
+					$(`#${joyoList.grade[grade_i][kanji_i].kanji}`).addClass("kanji_item_active");
+				}
+				else
+					$(`#${joyoList.grade[grade_i][kanji_i].kanji}`).removeClass("kanji_item_active");
+			}
+		}
+
+		// Detta funkar åt ett håll
+		// for (let rtk_i = 0; rtk_i <= _qtyLearnt; rtk_i++) {
+		// 	for (let grade_i = 1; grade_i < joyoList.grade.length; grade_i++) {
+		// 		for (let kanji_i = 0; kanji_i < joyoList.grade[grade_i].length; kanji_i++) {
+		// 			if (rtk_i == joyoList.grade[grade_i][kanji_i].heisigIndex) {
+		// 				$(`#${joyoList.grade[grade_i][kanji_i].kanji}`).addClass("kanji_item_active");
+		// 			}
+		// 		}
+		// 	}
+		// }
+	});
 });
 
-function badTomfoolery(element) {
-	element.form.amountInput.value = element.value;
-	_qtyLearnt = element.value;
-	console.log("この世界を再建する", _qtyLearnt);
-	rebuildThisWorld(element.value);
-}
 
-function rebuildThisWorld() {
-	resetRtkList(_rtkList);
-	setLearntRtkList(_rtkList, _qtyLearnt);
-	resetJoyoList(_joyoList); // Istället för att köra denna borde du göra en egen funktion som bara resettar "isActive"
-	compareRtkJoyo(_rtkList, _joyoList);
-
-	$("#grade_list").empty();
-	///////////// Bygga bygga
-	for (let i = 1; i < _joyoList.grade.length; i++) {
-		let grade_section = $("<section>").addClass("grade");
-		let grade_h2 = $("<h3>").text(`Grade ${i == 7 ? "7-12" : i}`);
-		let grade_kanji_container = $("<div>").addClass("kanji_container");
-
-		grade_section.append(grade_h2);
-		for (let j = 0; j < _joyoList.grade[i].length; j++) { // Det vackraste jag sett
-			grade_kanji_container.append(`<span class="kanji_item ${_joyoList.grade[i][j].isActive ? 'kanji_item_active' : ''}">
-											<a href='//${_joyoList.grade[i][j].jishoHref}'>
-												${_joyoList.grade[i][j].kanji}
-											</a>
-										</span>`);
-		}
-		grade_section.append(grade_kanji_container);
-		$("#grade_list").append(grade_section);
-	}
-}
 
 // Hjälp funktioner, かな
 function resetRtkList(rtkList) {
@@ -86,7 +88,8 @@ function resetJoyoList(joyoList) {
 									strokes: parseInt(joyoList.grade[i][j].strokes),
 									jishoHref: joyoList.grade[i][j].jishoHref,
 									koohiiHref: joyoList.grade[i][j].koohiiHref,
-									isActive: false};
+									isActive: false,
+									heisigIndex: joyoList.grade[i][j].heisigIndex};
 }
 
 // range ex: 500 (0-500 kanji learnt)
@@ -95,27 +98,26 @@ function setLearntRtkList(rtkList, range) {
 		rtkList[i].learnt = true;
 }
 
-function compareRtkJoyo(rtkList, joyoList) {
+function compareRtkJoyo(rtkList, joyoList, range) {
 	let qtyRtkExisted = 0;
-	for (let rtk_i = 0; rtk_i < _qtyLearnt; rtk_i++) {
+	for (let rtk_i = 0; rtk_i < range; rtk_i++) {
 		for (let grade_i = 1; grade_i < joyoList.grade.length; grade_i++) {
 			for (let kanji_i = 0; kanji_i < joyoList.grade[grade_i].length; kanji_i++) {
 				if (rtkList[rtk_i].kanji == joyoList.grade[grade_i][kanji_i].kanji) {
-					rtkList[rtk_i].learnt = false;
+					rtkList[rtk_i].learnt = false; // Tänkte säga att jag kanske inte ens behöver rtkList om jag ändå har heisigIndex i joyoList, men då kommer inte detta funka, kuken
 					joyoList.grade[grade_i][kanji_i].isActive = true;
-					console.log("lmao");
 					qtyRtkExisted++;
 				}
 			}
 		}
 	}
 	let rtkNotInJoyo = new Array();
-	for (let i = 0; i < _qtyLearnt; i++) {
+	for (let i = 0; i < range; i++) {
 		if (rtkList[i].learnt == true)
 			rtkNotInJoyo.push(rtkList[i].kanji);
 	}
-	if (qtyRtkExisted + rtkNotInJoyo.length != _qtyLearnt) {
-		console.log("qtyRtkExisted: " + qtyRtkExisted + " + rtkNotInJoyo: " + rtkNotInJoyo.length + " does not equal qtyLearnt: " + _qtyLearnt);
+	if (qtyRtkExisted + rtkNotInJoyo.length != range) {
+		console.log("qtyRtkExisted: " + qtyRtkExisted + " + rtkNotInJoyo: " + rtkNotInJoyo.length + " does not equal _qtyLearnt: " + range);
 		return 1;
 	}
 	return {qtyRtkExisted, rtkNotInJoyo};
